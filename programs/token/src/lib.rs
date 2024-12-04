70a6e5acc8e10b639153d2902de122ea1f227fc8
kox <garsanzi@gmail.com>
2024-12-04 13:08:13 +0100

    adding metaplex core program

--- programs/token/src/lib.rs
+++ programs/token/src/lib.rs
@@ -1,4 +1,4 @@
-//! Last updated at mainnet-beta slot height: 298486032
+//! Last updated at mainnet-beta slot height: 305349503
 
 #[cfg(feature = "associated-token")]
 pub mod associated_token;
@@ -6,3 +6,5 @@ pub mod associated_token;
 pub mod token;
 #[cfg(feature = "token-2022")]
 pub mod token2022;
+#[cfg(feature = "mpl-core")]
+pub mod mpl_core;
--- programs/token/update.sh
+++ programs/token/update.sh
@@ -3,4 +3,14 @@
 solana program dump TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA ./src/elf/token.so -u mainnet-beta
 solana program dump TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb ./src/elf/token_2022.so -u mainnet-beta
 solana program dump ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL ./src/elf/associated_token.so -u mainnet-beta
-solana slot -u mainnet-beta | xargs -I {} sed -i '' 's|//! Last updated at mainnet-beta slot height: .*|//! Last updated at mainnet-beta slot height: {}|' ./src/lib.rs
+solana program dump CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d ./src/elf/mpl_core.so -u mainnet-beta
+
+slot=$(solana slot -u mainnet-beta)
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    sed -i '' "s|//! Last updated at mainnet-beta slot height: .*|//! Last updated at mainnet-beta slot height: $slot|" ./src/lib.rs
+else
+    # Linux
+    sed -i "s|//! Last updated at mainnet-beta slot height: .*|//! Last updated at mainnet-beta slot height: $slot|" ./src/lib.rs
+fi
+
